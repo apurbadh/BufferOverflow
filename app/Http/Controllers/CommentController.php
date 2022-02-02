e3b74887b2960285114416884cd99e5fc34f85fa
Apurba Adhikari <erikduke182@gmail.com>
2022-02-02 17:07:06 +0545

    Controllers created

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -8,13 +8,16 @@ use Illuminate\Http\Request;
 class CommentController extends Controller
 {
 
-    public function create()
+    public function create(Request $request): \Illuminate\Http\RedirectResponse
     {
-
+        $data = $request->validate(Comment::$rules);
+        Comment::create($data);
+        return back()->with('message', 'Commented Successfully !');
     }
 
-    public function delete(Comment $comment)
+    public function delete(Comment $comment): \Illuminate\Http\RedirectResponse
     {
-
+        $comment->delete();
+        return back()->with('message', 'Deleted Successfully !');
     }
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
@@ -15,31 +16,40 @@ class PostController extends Controller
 
     public function show(Post $post)
     {
-
+        return view('post.post', compact('post'));
     }
 
     public function create()
     {
-
+        return view('post.create');
     }
 
-    public function store()
+    public function store(Request $request): \Illuminate\Http\RedirectResponse
     {
-
+        $data = $request->validate(Post::$rules);
+        $data['user_id'] = Auth::id();
+        Post::create($data);
+        return back()->with('message', 'Post Created Successfully !');
     }
 
     public function edit(Post $post)
     {
-
+        return view('post.edit', compact('post'));
     }
 
-    public function update(Post $post)
+    public function update(Post $post, Request $request): \Illuminate\Http\RedirectResponse
     {
-
+        $data = $request->validate([
+            'title' => 'required|unique:posts,title,'.$post->id,
+            'code' => 'required'
+        ]);
+        $post->update($data);
+        return back()->with('message', 'Updated Successfully !');
     }
 
     public function delete(Post $post)
     {
-
+        $post->delete();
+        return redirect('/')->with('message', 'Deleted Successfully !');
     }
 }
